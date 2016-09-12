41177ae240769d1097c9ddb445eda73921593db4
M. Shulhan <ms@kilabit.info>
2016-09-12 07:56:18 +0700

    Install script to update list of ads/malware hosts file
    User now can run this script manually, to update their hosts file without
    waiting update from rescached package.

--- src/scripts/rescached-update-hosts-block.sh
+++ src/scripts/rescached-update-hosts-block.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-HOSTS_BLOCK=hosts.block
+HOSTS_BLOCK=/etc/rescached/hosts.block
+
+if [[ $${UID} != 0 ]]; then
+	echo "[rescached] User root needed for overwriting ${HOSTS_BLOCK}!"
+	exit 1
+fi
 
 wget -O - "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate[day]=&startdate[month]=&startdate[year]=&mimetype=plaintext" > $HOSTS_BLOCK
 wget -O - "http://www.malwaredomainlist.com/hostslist/hosts.txt" >> $HOSTS_BLOCK
